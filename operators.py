67348e1c7745f495ad730ce063b19d5072e264e1
kadebrenda122-arch <kadebrenda122@gmail.com>
2026-08-31 13:31:18 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,30 @@
+number1= 6
+number2=5
+
+#addition
+sum = number1 + number2
+print(sum)
+
+#subtraction
+subtraction = number1-number2
+print(subtraction)
+
+#modulus
+modulus = number1 % number2
+print(modulus)
+
+print("The first number is {number1}, and the second number is{number2}")
+x = 10 + 3 * 2** 2
+print(x)
+x = (2+3) * 10-3
+print(x)
+X = 2.9
+print(round(X))
+print(abs(-2.9))
+print("Ramba Brenda")
+print('O----')
+print(' | | | |')
+print('*' * 10)
+name = input('what is your name? ')
+print('Hi'+ name )
+
